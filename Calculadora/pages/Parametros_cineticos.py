2dd590e2ce4480176a76b1ebb64355d5dce7e5c6
Trinidad Martí Gutiérrez <trinidad.martig@gmail.com>
2024-11-09 15:38:21 -0300

    se añade uso condicional de formulas y se modifica orden de columnas

---
+++ Calculadora/pages/Parametros_cineticos.py
@@ -0,0 +1,95 @@
+import streamlit as st
+import math
+
+# Título de la aplicación
+st.title("Parámetros cinéticos 📈📉")
+
+# Columnas para organizar la entrada de datos
+col1, col_gap, col2 = st.columns([1.5, 1, 2.5])
+
+with col1:
+    # Inputs
+    peso = st.number_input("Peso (kg)", min_value=1.0, value=70.0)
+    talla = st.number_input("Talla (cm)", min_value=1, value=170)
+    conc_peak = st.number_input("Concentración Peak", min_value=0.0, value=41.6)
+    conc_basal = st.number_input("Concentración Basal", min_value=0.0, value=15.8)
+    t_ini_dosis = st.number_input("Tiempo inicial dosis (hrs)", min_value=0.0, value=2.0)
+    t_ini_dosis_2 = st.number_input("Tiempo final dosis (hrs)", min_value=0.0, value=12.0)
+
+with col2:
+    # Inputs
+    t_infusion = st.number_input("Tiempo de infusión (hrs)", min_value=0.0, value=2.0)
+    intervalo = st.number_input("Intervalo entre dosis (hrs)", min_value=1, value=12)
+    cim = st.number_input("Concentración mínima inhibitoria (CIM)", min_value=0.0, value=1.0)
+    dosis = st.number_input("Dosis (mg)", min_value=0, value=1000)
+
+# Funciones
+
+def get_dosis_dia(dosis, intervalo, peso):
+    """Dosis diaria mg/kg/día"""
+    formula = (dosis * (24 / intervalo)) / peso
+    return round(formula, 2)
+
+def get_const_eliminacion(conc_peak, conc_basal, t_ini_dosis, t_ini_dosis_2):
+    """Constante de eliminación"""
+    formula = math.log(conc_peak / conc_basal) / (t_ini_dosis_2 - t_ini_dosis)
+    return round(formula, 3)
+
+def get_concentracion_maxima_real(conc_peak, conc_basal, t_ini_dosis, t_ini_dosis_2, t_infusion):
+    """Concentración máxima real"""
+    const_eliminacion = get_const_eliminacion(conc_peak, conc_basal, t_ini_dosis, t_ini_dosis_2)
+    formula = conc_peak / math.exp(-const_eliminacion * (t_ini_dosis - t_infusion))
+    return round(formula, 2)
+
+def get_concentracion_minima_real(conc_peak, conc_basal, t_ini_dosis, t_ini_dosis_2, intervalo, t_infusion):
+    """Concentración mínima real"""
+    conc_max_real = get_concentracion_maxima_real(conc_peak, conc_basal, t_ini_dosis, t_ini_dosis_2, t_infusion)
+    const_eliminacion = get_const_eliminacion(conc_peak, conc_basal, t_ini_dosis, t_ini_dosis_2)
+    formula = conc_max_real * math.exp(-const_eliminacion * (intervalo - t_infusion))
+    return round(formula, 1)
+
+def get_aclaramiento_medicamento(conc_peak, conc_basal, t_ini_dosis, t_ini_dosis_2, dosis, t_infusion, intervalo):
+    """Aclaramiento del medicamento"""
+    vol_distribucion = get_volumen_distribucion(conc_peak, conc_basal, t_ini_dosis, t_ini_dosis_2, dosis, t_infusion, intervalo)
+    const_eliminacion = get_const_eliminacion(conc_peak, conc_basal, t_ini_dosis, t_ini_dosis_2)
+    formula = (vol_distribucion * const_eliminacion) * 1000 / 60
+    return round(formula, 2)
+
+def get_volumen_distribucion(conc_peak, conc_basal, t_ini_dosis, t_ini_dosis_2, dosis, t_infusion, intervalo):
+    """Volumen de distribución"""
+    const_eliminacion = get_const_eliminacion(conc_peak, conc_basal, t_ini_dosis, t_ini_dosis_2)
+    conc_max_real = get_concentracion_maxima_real(conc_peak, conc_basal, t_ini_dosis, t_ini_dosis_2, t_infusion)
+    conc_min_real = get_concentracion_minima_real(conc_peak, conc_basal, t_ini_dosis, t_ini_dosis_2, intervalo, t_infusion)
+    formula = (dosis * (1 - math.exp(-const_eliminacion * t_infusion))) / (t_infusion * const_eliminacion * (conc_max_real - conc_min_real * math.exp(-const_eliminacion * t_infusion)))
+    return round(formula, 2)
+
+# Mostrar resultados en la columna inferior
+st.header("Resultados 🧾✍🏼")
+col3, col_gap2, col4 = st.columns([1.5, 1, 2.5])
+
+with col3:
+    
+    # Cálculo y visualización de la dosis diaria
+    dosis_dia = get_dosis_dia(dosis, intervalo, peso)
+    st.markdown(f"Dosis diaria: :green-background[**{dosis_dia} mg/kg/día**]")
+    
+    # Cálculo y visualización de la constante de eliminación
+    const_eliminacion = get_const_eliminacion(conc_peak, conc_basal, t_ini_dosis, t_ini_dosis_2)
+    st.markdown(f"Constante de eliminación: :green-background[**{const_eliminacion}**]")
+    
+with col4:
+    # Cálculo y visualización de la concentración máxima real
+    conc_max_real = get_concentracion_maxima_real(conc_peak, conc_basal, t_ini_dosis, t_ini_dosis_2, t_infusion)
+    st.markdown(f"Concentración máxima real: :green-background[**{conc_max_real} mg/L**]")
+    
+    # Cálculo y visualización de la concentración mínima real
+    conc_min_real = get_concentracion_minima_real(conc_peak, conc_basal, t_ini_dosis, t_ini_dosis_2, intervalo, t_infusion)
+    st.markdown(f"Concentración mínima real: :green-background[**{conc_min_real} mg/L**]")
+    
+    # Cálculo y visualización del volumen de distribución
+    vol_dist = get_volumen_distribucion(conc_peak, conc_basal, t_ini_dosis, t_ini_dosis_2, dosis, t_infusion, intervalo)
+    st.markdown(f"Volumen de distribución : :green-background[**{vol_dist} L**]")
+    
+    # Cálculo y visualización del aclaramiento del medicamento
+    aclaramiento = get_aclaramiento_medicamento(conc_peak, conc_basal, t_ini_dosis, t_ini_dosis_2, dosis, t_infusion, intervalo)
+    st.markdown(f"Aclaramiento del medicamento: :green-background[**{aclaramiento} L/h**]")
--- Calculadora/pages/VFG.py
+++ Calculadora/pages/VFG.py
@@ -1,32 +1,25 @@
 import streamlit as st
 
 # Configuración de la página
-st.title("Calculadora VFG")
+st.title("Calculadora VFG 📊")
 
-# Inputs comunes
-col1, col2 = st.columns([2, 2])  # Crear dos columnas
+# Inputs comunes en dos columnas
+col1, col2 = st.columns(2)
 
-with col1:  # Columna para inputs
-    # Inputs para las fórmulas
-    st.header("Selecciona una fórmula")
-    formula_opcion = st.selectbox(
-        "Fórmulas disponibles:",
-        ["Clearance de creatinina (Cockcroft-Gault)", 
-         "VFG no normalizada (CKD-EPI 2021)", 
-         "VFG no normalizada (CKD-EPI-cistatina 2021)",
-         "BIS 1"]
-    )
-    edad = st.number_input("Edad (años)", min_value=0 )
-    peso = st.number_input("Peso (kg)", min_value=0.0 )
-    talla = st.number_input("Talla (cm)", min_value=0.0)
+with col1:
+    edad = st.number_input("Edad (años)", min_value=0, format="%d")
+    peso = st.number_input("Peso (kg)", min_value=0.0, format="%.0f")
     genero = st.selectbox("Género", ["masculino", "femenino"])
-    creatinina = st.number_input("Creatinina (mg/dL)", min_value=0.0)
-    if formula_opcion == "VFG no normalizada (CKD-EPI-cistatina 2021)":
-        cistatina = st.number_input("Cistatina (mg/dL)", min_value=0.0)
+
+with col2:
+    talla = st.number_input("Talla (cm)", min_value=0.0, format="%.0f")
+    creatinina = st.number_input("Creatinina (mg/dL)", min_value=0.0, format="%.0f")
+    cistatina = st.number_input("Cistatina (mg/dL)", min_value=0.0, format="%.0f", key="cistatina_input")
+
+# Constantes y variables
+DECIMALS = 1
 
 # Funciones de cálculo
-DECIMALS = 3
-
 def bis1(creatinina, edad, genero):
     """ VFG BIS 1"""
     multi_genero = 1 if genero == "masculino" else 0.82
@@ -43,20 +36,23 @@ def get_peso_magro(genero, peso, imc):
     formula = (9270 * peso) / (ind1 + (ind2 * imc))
     return round(formula, DECIMALS)
 
+# Función para calcular el IMC con manejo de errores
 def get_imc(peso, talla):
-    return round(peso / ((talla * talla) / 10000), DECIMALS)
+    try:
+        if peso is None or talla is None or talla == 0:
+            raise ValueError("Ingrese los valores del paciente.")
+        return round(peso / ((talla * talla) / 10000), DECIMALS)
+    except ValueError as e:
+        st.error(str(e))
+        return None
 
 def get_superficie_corp(peso, talla):
     return (peso ** 0.425) * (talla ** 0.725) * 0.007184
 
 def get_cg_clearance_creatinina(edad, peso, talla, genero, creatinina):
     imc = get_imc(peso, talla)
-    _peso = None
+    _peso = get_peso_magro(genero, peso, imc) if imc > 30 else peso
     multi_genero = 1 if genero == "masculino" else 0.85
-    if imc > 30:
-        _peso = get_peso_magro(genero, peso, imc)
-    else:
-        _peso = peso
     total = ((140 - edad) * _peso / (72 * creatinina)) * multi_genero
     return round(total, 1)
 
@@ -67,19 +63,11 @@ def get_cg_estimacion_vfg(edad, peso, talla, genero, creatinina):
 
 def get_cdk_epicreatinina_2021(creatinina, genero, edad):
     if genero == "masculino":
-        var_genero_a = 0.9
-        multi_genero = 1
-        if creatinina <= 0.9:
-            var_genero_b = -0.302
-        else:
-            var_genero_b = -1.2
+        var_genero_a, multi_genero = 0.9, 1
+        var_genero_b = -0.302 if creatinina <= 0.9 else -1.2
     else:
-        var_genero_a = 0.7
-        multi_genero = 1.012
-        if creatinina <= 0.7:
-            var_genero_b = -0.241
-        else:
-            var_genero_b = -1.2
+        var_genero_a, multi_genero = 0.7, 1.012
+        var_genero_b = -0.241 if creatinina <= 0.7 else -1.2
     formula = 142 * ((creatinina / var_genero_a) ** var_genero_b) * (0.9938 ** edad) * multi_genero
     return round(formula, 1)
 
@@ -90,53 +78,24 @@ def get_vfg_no_normalizada(creatinina, genero, edad, peso, talla):
     return round(formula, 1)
 
 def get_ckd_epicrea_cys_2021(creatinina, cistatina, genero, edad):
+    if cistatina <= 0:
+        # Return None or some indication that the result is unavailable
+        return None
     multi_genero = 1 if genero == "masculino" else 0.963
     if genero == "masculino":
         if creatinina <= 0.9:
-            if cistatina <= 0.8:
-                A = 0.9
-                B = -0.144
-                C = 0.8
-                D = -0.323
-            else:
-                A = 0.9
-                B = -0.144
-                C = 0.8
-                D = -0.778
+            A, B = 0.9, -0.144
+            C, D = (0.8, -0.323) if cistatina <= 0.8 else (0.8, -0.778)
         else:
-            if cistatina <= 0.8:
-                A = 0.9
-                B = -0.544
-                C = 0.8
-                D = -0.323
-            else:
-                A = 0.9
-                B = -0.544
-                C = 0.8
-                D = -0.778
+            A, B = 0.9, -0.544
+            C, D = (0.8, -0.323) if cistatina <= 0.8 else (0.8, -0.778)
     else:
         if creatinina <= 0.7:
-            if cistatina <= 0.8:
-                A = 0.7
-                B = -0.219
-                C = 0.8
-                D = -0.323
-            else:
-                A = 0.7
-                B = -0.219
-                C = 0.8
-                D = -0.778
+            A, B = 0.7, -0.219
+            C, D = (0.8, -0.323) if cistatina <= 0.8 else (0.8, -0.778)
         else:
-            if cistatina <= 0.8:
-                A = 0.7
-                B = -0.544
-                C = 0.8
-                D = -0.323
-            else:
-                A = 0.7
-                B = -0.544
-                C = 0.8
-                D = -0.778
+            A, B = 0.7, -0.544
+            C, D = (0.8, -0.323) if cistatina <= 0.8 else (0.8, -0.778)
     formula = 135 * ((creatinina / A) ** B) * ((cistatina / C) ** D) * (0.9961 ** edad) * multi_genero
     return round(formula, 1)
 
@@ -146,33 +105,66 @@ def get_ckd_epicrea_cys_vfg_no_norm(creatinina, cistatina, genero, edad, peso, t
     formula = (superficie_corp / 1.73) * estimacion_vfg
     return round(formula, 1)
 
-# Cálculos y resultados según la fórmula seleccionada
-resultados = {}  # Diccionario para almacenar resultados
+# Cálculo de IMC
+imc = get_imc(peso, talla)
+# Alerta para peso magro si el IMC es mayor a 30
+if imc and imc > 30:
+    st.warning(f"Paciente con IMC de {imc}. Preferir Cockcroft-Gault 📢🚨❗")
+    st.warning(f"Peso magro: {get_peso_magro(genero, peso, imc)} Kg ❗")
+# Inicializa resultados
+clear_creatinina  = estimacion_vfg_cg = estimacion_vfg = estimacion_vfg_cys = vfg_no_normalizada = vfg_no_normalizada_cistatina = bis_1_resultado = None
 
-# Verificación de entradas antes de realizar cálculos
-if edad <= 0 or peso <= 0 or talla <= 0 or creatinina <= 0 or cistatina <=0:
-    st.warning("Por favor, asegúrate de que todos los valores sean mayores que cero.")
-else:
-    if formula_opcion == "Clearance de creatinina (Cockcroft-Gault)":
+# Verificación de condiciones y cálculos
+if edad > 0 and peso > 0 and talla > 0 and creatinina > 0:
+    if edad <= 70:
         clear_creatinina = get_cg_clearance_creatinina(edad, peso, talla, genero, creatinina)
-        estimacion_vfg = get_cg_estimacion_vfg(edad, peso, talla, genero, creatinina)
-        resultados["Clearance de creatinina"] = f"{clear_creatinina} mL/min"
-        resultados["Estimación de VFG"] = f"{estimacion_vfg} mL/min/1.73m²"
-
-    elif formula_opcion == "VFG no normalizada (CKD-EPI 2021)":
-        vfg_no_normalizada = get_vfg_no_normalizada(creatinina, genero, edad, peso, talla)
-        resultados["VFG no normalizada"] = f"{vfg_no_normalizada} mL/min/1.73m²"
-
-    elif formula_opcion == "VFG no normalizada (CKD-EPI-cistatina 2021)":
+        estimacion_vfg_cg = get_cg_estimacion_vfg(edad, peso, talla, genero, creatinina)
+    vfg_no_normalizada = get_vfg_no_normalizada(creatinina, genero, edad, peso, talla)
+    estimacion_vfg = get_cdk_epicreatinina_2021(creatinina, genero, edad)
+    estimacion_vfg_cys = get_ckd_epicrea_cys_2021(creatinina, cistatina, genero, edad)
+    if cistatina > 0:
         vfg_no_normalizada_cistatina = get_ckd_epicrea_cys_vfg_no_norm(creatinina, cistatina, genero, edad, peso, talla)
-        resultados["VFG no normalizada (CKD-EPI-cistatina)"] = f"{vfg_no_normalizada_cistatina} mL/min/1.73m²"
+    bis_1_resultado = bis1(creatinina, edad, genero)
 
-    elif formula_opcion == "BIS 1":
-        vfg_bis1 = bis1(creatinina, edad, genero)
-        resultados["VFG BIS 1"] = f"{vfg_bis1} mL/min"
 
-# Mostrar resultados
-with col2:  # Columna para resultados
-    st.subheader("Resultados")  # Encabezado de resultados
-    for key, value in resultados.items():
-        st.write(f"{key}: {value}")
+# Función para mostrar resultados con color
+def mostrar_resultado(nombre, valor):
+    if valor is None:
+        # Texto en rojo si el valor es None
+        st.markdown(f"{nombre}:  :red-background[**Sin resultado**]")
+    elif nombre == 'VFG no normalizada (CKD-EPI 2021)' or nombre == 'VFG no normalizada (CKD-EPI-cistatina 2021)':
+        # Texto en verde si el valor tiene un resultado
+        st.markdown(f"{nombre}: :green-background[**{valor} mL/min**]")
+    else:
+        # Texto en verde si el valor tiene un resultado
+        st.markdown(f"{nombre}: :green-background[**{valor} mL/min/1.73m²**]")
+
+# Definir el diseño de subcolumnas con títulos
+st.markdown("### Cockcroft-Gault")
+col_a, col_b = st.columns(2)
+with col_a:
+    mostrar_resultado("Estimación de VFG", estimacion_vfg_cg)
+with col_b:
+    mostrar_resultado("Clearance de creatinina", clear_creatinina)
+
+st.markdown("### CKD-EPI Creatinina (2021)")
+col_c, col_d = st.columns(2)
+with col_c:
+    mostrar_resultado("Estimación VFG", estimacion_vfg)
+with col_d:
+    mostrar_resultado("VFG no normalizada", vfg_no_normalizada)
+
+st.markdown("### CKD-EPI Crea-cys (2021)")
+col_e, col_f = st.columns(2)
+with col_e:
+    mostrar_resultado("VFG no normalizada", vfg_no_normalizada_cistatina)
+with col_f:
+    mostrar_resultado("Estimación VFG", estimacion_vfg_cys)
+
+st.markdown("### BIS-1")
+col_e = st.columns(1)[0]
+with col_e:
+    mostrar_resultado("VFG BIS 1", bis_1_resultado)
+
+
+
