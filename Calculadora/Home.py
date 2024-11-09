1ee9fd04f3b7cb1733477d0df8182293319d5570
Trinidad Martí Gutiérrez <trinidad.martig@gmail.com>
2024-11-09 16:42:44 -0300

    se añade fondo y se configura vista oscura

--- Calculadora/Home.py
+++ Calculadora/Home.py
@@ -1,5 +1,27 @@
 import streamlit as st
 
-st.markdown("# Mi Calculadora de Fórmulas 🧮")
+def set_bg_hack_url():
+    '''
+    A function to unpack an image from url and set as bg.
+    Returns
+    -------
+    The background.
+    '''
+        
+    st.markdown(
+         f"""
+         <style>
+         .stApp {{
+             background: url("https://miro.medium.com/v2/resize:fit:1400/0*IVtTYMJpv3IcZVvR");
+             background-size: cover
+         }}
+         </style>
+         """,
+         unsafe_allow_html=True
+     )
+
+set_bg_hack_url()
+
+st.markdown("# Calculadora Clínica 🧮")
 st.markdown("Por favor, selecciona una fórmula en el menú de la izquierda para comenzar.")
-st.markdown("Desarrollado por: [Nebula CodeWorks](https://www.nebulacodeworks.io)")
+st.markdown("Desarrollado por: [Nebula CodeWorks](https://www.nebulacodeworks.io) 👩🏻‍💻🧑🏼‍💻")
