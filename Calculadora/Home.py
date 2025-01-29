31046ce0a644b48471542acaf83f4017422841fe
Trinidad Martí Gutiérrez <trinidad.martig@gmail.com>
2025-01-29 14:53:28 -0300

    se remueve fondo

--- Calculadora/Home.py
+++ Calculadora/Home.py
@@ -1,26 +1,26 @@
 import streamlit as st
 
-def set_bg_hack_url():
-    '''
-    A function to unpack an image from url and set as bg.
-    Returns
-    -------
-    The background.
-    '''
+# def set_bg_hack_url():
+#     '''
+#     A function to unpack an image from url and set as bg.
+#     Returns
+#     -------
+#     The background.
+#     '''
         
-    st.markdown(
-         f"""
-         <style>
-         .stApp {{
-             background: url("https://miro.medium.com/v2/resize:fit:1400/0*IVtTYMJpv3IcZVvR");
-             background-size: cover
-         }}
-         </style>
-         """,
-         unsafe_allow_html=True
-     )
+#     st.markdown(
+#          f"""
+#          <style>
+#          .stApp {{
+#              background: url("https://miro.medium.com/v2/resize:fit:1400/0*IVtTYMJpv3IcZVvR");
+#              background-size: cover
+#          }}
+#          </style>
+#          """,
+#          unsafe_allow_html=True
+#      )
 
-set_bg_hack_url()
+# set_bg_hack_url()
 
 st.markdown("# Calculadora Clínica 🧮")
 st.markdown("Por favor, selecciona una fórmula en el menú de la izquierda para comenzar.")
